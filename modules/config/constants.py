52986b1dbfb74b080e702321692f82c955bc25af
Vargur <40493346+VargurStorm@users.noreply.github.com>
2024-03-02 12:08:00 +0000

    Added better img_download retry system

--- modules/config/constants.py
+++ modules/config/constants.py
@@ -2,3 +2,8 @@ class Paths:
     LOG_PATH = "./application.log"
     CHAR_IMG_PATH = "./static/char_images"
     SHIP_IMG_PATH = "./static/ship_images"
+
+
+class Constants:
+    IMG_DOWNLOAD_RETIRES = 2
+    IMG_DOWNLOAD_SLEEP_TIME = 0.75
--- modules/controllers/texture_controller.py
+++ modules/controllers/texture_controller.py
@@ -3,6 +3,7 @@ import time
 from modules.models.wreck import Wreck
 from modules.config import logger
 from modules.graphics.graphics_engine import GraphicsEngine
+from modules.config.constants import Constants
 
 logger = logger.setup_logging()
 
@@ -19,9 +20,7 @@ def create_mgl_textures_from_wreck_list(app: GraphicsEngine, tex_type: str, wrec
     if tex_type == 'ship':
         texture_dict = app.mesh.texture.textures
         for wreck in wreck_list:
-            wreck.populate_ship_img()
-            time.sleep(0.75)
-            wreck.populate_ship_img()
+            image_download(wreck, tex_type, Constants.IMG_DOWNLOAD_RETIRES, Constants.IMG_DOWNLOAD_SLEEP_TIME)
             if wreck.ship_img_path:
                 ship_id = wreck.ship_id
                 mgl_texture = app.mesh.texture.get_texture(wreck.ship_img_path)
@@ -30,9 +29,7 @@ def create_mgl_textures_from_wreck_list(app: GraphicsEngine, tex_type: str, wrec
     if tex_type == 'char':
         texture_dict = app.mesh.texture.textures
         for wreck in wreck_list:
-            wreck.populate_char_img()
-            time.sleep(0.75)
-            wreck.populate_char_img()
+            image_download(wreck, tex_type, Constants.IMG_DOWNLOAD_RETIRES, Constants.IMG_DOWNLOAD_SLEEP_TIME)
             if wreck.char_img_path:
                 char_id = wreck.char_id
                 mgl_texture = app.mesh.texture.get_texture(wreck.char_img_path)
@@ -40,3 +37,21 @@ def create_mgl_textures_from_wreck_list(app: GraphicsEngine, tex_type: str, wrec
 
     else:
         logger.error(f"Texture type {tex_type} not recognized")
+
+
+def image_download(wreck: Wreck, image_type: str, retries: int, sleep_time: float):
+    if image_type == 'ship':
+        for i in range(retries):
+            wreck.populate_ship_img()
+            time.sleep(sleep_time)
+            if wreck.ship_img_path:
+                break
+    elif image_type == 'char':
+        for i in range(retries):
+            wreck.populate_char_img()
+            time.sleep(sleep_time)
+            if wreck.char_img_path:
+                break
+    else:
+        logger.error(f"Image type {image_type} not recognized")
+        return
